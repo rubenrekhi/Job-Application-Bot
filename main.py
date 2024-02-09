5786a7be87023442e0651f0b9171fb8a24ea24a0
Ruben <136935331+rubenrekhi@users.noreply.github.com>
2024-02-09 12:31:33 -0500

    Pivoted away from trying to sign into chrome profile and set up a terminal I/O where user can enter sign in details to their simplify account. Was able to successfully login and open the list of summer internships, however the program is crashing when I try to find the search bar to search for software jobs.

--- main.py
+++ main.py
@@ -2,25 +2,38 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time 
 from selenium.webdriver.chrome.options import Options
-
-
-chrome_options = Options()
-profile_path = '~/Users/rubenrekhi/Library/Application Support/Google/Chrome/Default'
-
-
-
-chrome_options.add_argument("--user-data-dir=/Users/rubenrekhi/Library/Application Support/Google/Chrome")
-chrome_options.add_argument("--profile-directory=Default")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 service = Service(executable_path="./chromedriver")
-driver = webdriver.Chrome(service=service, options=chrome_options)
+driver = webdriver.Chrome(service=service)
+
+
+driver.get("https://simplify.jobs/auth/login")
+time.sleep(3)
+email_input = driver.find_element(By.ID, "email")
+email = input("Enter your simplify login email")
+
+email_input.send_keys(email)
+
+pass_input = driver.find_element(By.ID, "password")
+password = input("Enter your simplify password")
+pass_input.send_keys(password + Keys.ENTER)
+time.sleep(10)
+
 
 
 
 driver.get("https://simplify.jobs/l/Top-Summer-Internships")
-#driver.get('https://google.com')
-time.sleep(10)
+time.sleep(3)
+search_jobs = driver.find_element(By.CLASS_NAME, "block h-11 w-full rounded-md border-gray-300 pl-10 text-base leading-5 transition focus:border-primary-base focus:outline-none focus:ring-4 focus:ring-primary-lightest")
+search_jobs.send_keys("Software" + Keys.ENTER)
+
+
+time.sleep(3)
+
+
+
+
 driver.quit()
 
